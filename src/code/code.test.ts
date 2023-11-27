889f65f7bfd82892c4f99dfd9f430562df1a4f20
Lukas Keller <65132460+Hebniki@users.noreply.github.com>
2023-11-27 13:48:48 +0000

    Pushing some tests and updated index.ts file

--- src/code/code.test.ts
+++ src/code/code.test.ts
@@ -1,9 +1,18 @@
 import { test, describe, expect } from "vitest";
-import { add } from "./index";
+import { add, multiply, sum } from "./index";
 
 describe("Basic tests", () => {
   test("adds two numbers", () => {
     expect(add(1, 2)).toEqual(3);
     expect(add(2, 2)).toEqual(4);
+  });
+  test("multiplies two numbers", () => {
+    expect(multiply(1, 2)).toEqual(2);
+    expect(multiply(2, 2)).toEqual(4);
+  });
+  test("Sum an array of numbers", () => {
+    // expect(sum([1, 2, 3])).toEqual(6);
+    // expect(sum([1, 2, 3, 4])).toEqual(10);
+    expect(sum([5, 8, 37])).toEqual(50);
   });
 });
